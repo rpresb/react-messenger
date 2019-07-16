8d3fac246b214fc52bfca869642fdfe2b62d64da
Rodrigo De Presbiteris <rodrigo.depresbiteris@travelportdigital.com>
2019-07-16 11:17:10 +0100

    Craete router and add dependencies

---
+++ src/AppRouter.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from './scenes/Home';
+import Login from './scenes/Login';
+
+const AppRouter = () => {
+  return (
+    <Router>
+      <Route path="/" exact component={Home} />
+      <Route path="/login" component={Login} />
+    </Router>
+  );
+};
+
+export default AppRouter;
